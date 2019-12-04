faa254bd99bef5fc17dfa861ecc703537a24574c
wtait <wade.tait@gmail.com>
2019-12-03 20:12:55 -0500

    corrects spelling of AddStakeCall() function reference

--- src/mappingTokenFunctions.ts
+++ src/mappingTokenFunctions.ts
@@ -1,7 +1,7 @@
-import { addStake } from "../generated/TokenFunctionsContract/Contract"
+import { AddStakeCall } from "../generated/TokenFunctionsContract/Contract"
 import { Stake } from "../generated/schema"
 
-export function handleAddStake(call: addStake): void {
+export function handleAddStake(call: AddStakeCall): void {
     const id = call.transaction.hash.toHex();
     let entity = Stake.load(id)
     if (entity == null) {
