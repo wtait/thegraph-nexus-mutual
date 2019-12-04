fb638f345b4b89f5cc79c4e96eb7a94e9b8272c3
danielheyman <daniel.heyman@gmail.com>
2019-12-03 21:33:42 -0500

    Ignore old contracts

--- src/mappingMemberRole.ts
+++ src/mappingMemberRole.ts
@@ -1,7 +1,13 @@
 import { PayJoiningFeeCall } from "../generated/templates/MemberRoles/MemberRoles"
-import { Member } from "../generated/schema"
+import { Member, MasterContract } from "../generated/schema"
+import { log } from "@graphprotocol/graph-ts";
 
 export function handlePayJoiningFee(call: PayJoiningFeeCall): void {
+  let mc = MasterContract.load("1");
+  if (mc.memberRoles != call.to) {
+    log.info("Ignoring outdated memberRole contract: {}", [call.to.toHexString()]);
+    return;
+  }
   const id = call.inputs._userAddress.toHex();
   let entity = Member.load(id)
   if (entity == null) {
--- src/mappingPool1.ts
+++ src/mappingPool1.ts
@@ -1,7 +1,13 @@
 import { MakeCoverBeginCall } from "../generated/templates/Pool1/Pool1"
-import { Cover } from "../generated/schema"
+import { Cover, MasterContract } from "../generated/schema"
+import { log } from "@graphprotocol/graph-ts";
 
 export function handleNewCover(call: MakeCoverBeginCall): void {
+  let mc = MasterContract.load("1");
+  if (mc.pool1 != call.to) {
+    log.info("Ignoring outdated pool1 contract: {}", [call.to.toHexString()]);
+    return;
+  }
   const id = call.transaction.hash.toHex();
   let entity = Cover.load(id)
   if (entity == null) {
