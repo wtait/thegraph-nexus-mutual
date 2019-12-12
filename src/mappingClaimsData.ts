9c68f64c0defbf1ef2cbea1dcb68dcaf83a9c5ec
Wade <wade.tait@gmail.com>
2019-12-11 19:41:21 -0600

    ClaimsData source added to manifest
    modifies subgraph.yaml file to include a mapping source for ClaimsData.
    Creates src/mappingClaimsData.ts file with a scaffolding for addClaim,
    addClaimVoteMember handlers.

---
+++ src/mappingClaimsData.ts
@@ -0,0 +1,15 @@
+import { AddClaimCall, AddClaimVoteCACall, AddClaimVotememberCall} from "../generated/templates/ClaimsData/ClaimsData"
+
+
+
+export function handleAddClaim(call: AddClaimCall): void {
+
+}
+
+export function handleAddClaimVoteCACall(call: AddClaimVoteCACall) void {
+
+}
+
+export function handleAddClaimVotememberCall(call: AddClaimVotememberCall) void {
+    
+}
