e231c9e917eb8fba8875387dda5b1f9e822657e2
Wade <wade.tait@gmail.com>
2019-12-20 11:05:15 -0600

    playing with multiple apollo clients

--- dapp/src/index.js
+++ dapp/src/index.js
@@ -1,6 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import ApolloClient from "apollo-boost";
+import { ApolloProvider } from "react-apollo";
 import './index.css';
 import App from './App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+if (!process.env.REACT_APP_GRAPHQL_ENDPOINT) {
+    throw new Error('REACT_APP_GRAPHQL_ENDPOINT environment variable not defined')
+  }
+
+const nexusClient = new ApolloClient({
+    uri: process.env.REACT_APP_GRAPHQL_ENDPOINT,
+    cache: new InMemoryCache(),
+})
+
+ReactDOM.render(
+    <ApolloProvider client={nexusClient}>
+        <App />
+    </ApolloProvider>.
+     document.getElementById('root')
+);
